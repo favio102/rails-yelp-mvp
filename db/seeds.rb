b219c23f9c84dc048dcdb4fb5437b7d9f9d2a35a
Favio Valero <favio102@protonmail.com>
2023-07-03 05:03:54 -0700

    100 restaurant created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "cleaning data base...."
+Restaurant.destroy_all
+
+puts "Creating restaurants...."
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Faker::Restaurant.type
+  )
+  puts "restaurant with id: #{restaurant.id} has benn created!"
+end
+
+puts "Created!"
